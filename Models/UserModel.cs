c36552ccf46e0cb53873b1710f72643ae01b2c89
Kunal Kumar <kumarkunalkk0@gmail.com>
2026-07-22 11:33:58 +0530

    Enhance UserModel with validation attributes
    Added data annotations for validation to UserModel properties.

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -1,13 +1,24 @@
-﻿namespace UserAccountUI.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace UserAccountUI.Models
 {
     public class UserModel
     {
+        [Required]
         public string Name { get; set; }
 
+        [Required]
+        [Display(Name = "Login ID")]
         public string LoginId { get; set; }
 
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        [Display(Name = "Confirm Password")]
         public string ConfirmPassword { get; set; }
     }
 }
