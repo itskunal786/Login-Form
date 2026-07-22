87cc4e2dd0747b8cdba0c2bb45868b9196b8fcdc
Kunal <kumarkunalkk0@gmail.com>
2026-07-22 10:52:04 +0530

    Initial Commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserAccountUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿namespace UserAccountUI.Models
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+
+        public string LoginId { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+    }
+}
